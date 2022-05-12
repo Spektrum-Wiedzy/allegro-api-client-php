dc8fbeecf80c651317685aa5f73e033254479012
damian <damianchojnacki@op.pl>
2022-05-12 11:41:34 +0200

    updated fulfillment statuses

--- src/Model/CheckoutFormFulfillmentStatus.php
+++ src/Model/CheckoutFormFulfillmentStatus.php
@@ -48,8 +48,10 @@ class CheckoutFormFulfillmentStatus
     const _NEW = 'NEW';
     const PROCESSING = 'PROCESSING';
     const READY_FOR_SHIPMENT = 'READY_FOR_SHIPMENT';
+    const READY_FOR_PICKUP = 'READY_FOR_PICKUP';
     const SENT = 'SENT';
     const CANCELLED = 'CANCELLED';
+    const SUSPENDED = 'SUSPENDED';
 
     /**
      * Gets allowable values of the enum
@@ -61,8 +63,10 @@ class CheckoutFormFulfillmentStatus
             self::_NEW,
             self::PROCESSING,
             self::READY_FOR_SHIPMENT,
+            self::READY_FOR_PICKUP,
             self::SENT,
             self::CANCELLED,
+            self::SUSPENDED,
         ];
     }
 }
